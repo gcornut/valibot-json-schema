99210c84e3a3bd75a2f454545decc357c60e918b
Andreas Söderlund <ciscoheat@gmail.com>
2024-01-28 18:01:13 +0100

    Support for custom function.

--- src/toJSONSchema/validations.ts
+++ src/toJSONSchema/validations.ts
@@ -15,6 +15,7 @@ import {
     Ipv4Validation,
     Ipv6Validation,
     UuidValidation,
+    CustomValidation,
 } from 'valibot';
 import { assert } from '../utils/assert';
 import { JSONSchema7 } from 'json-schema';
@@ -35,7 +36,8 @@ export type SupportedValidation =
     | Ipv4Validation<any>
     | Ipv6Validation<any>
     | UuidValidation<any>
-    | EmailValidation<any>;
+    | EmailValidation<any>
+    | CustomValidation<any>;
 
 type ValidationConverter<V extends SupportedValidation> = (validation: V) => JSONSchema7;
 
@@ -73,6 +75,9 @@ const VALIDATION_BY_SCHEMA: {
     boolean: {
         value: ({ requirement }) => ({ const: requirement }),
     },
+    object: {
+        custom: () => ({})
+    }
 };
 
 /**
