1a1d3a18dd6613192f9ed8eff6c1693a6f34d448
Guillaume Cornut <guillaume.cornut@lumapps.com>
2024-09-04 11:15:08 +0200

    Export jsonSchemaMetadata

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { toJSONSchema } from './toJSONSchema';
 export { ToJSONSchemaOptions } from './toJSONSchema/types';
 export { withJSONSchemaFeatures } from './extension/withJSONSchemaFeatures';
+export { jsonSchemaMetadata } from './extension/jsonSchemaMetadata';
